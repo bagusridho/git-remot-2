3111cd582f4a0b09f546e382cc5458bff761e45f
Bagus Ridho <balap198@gmail.com>
2024-03-23 11:29:27 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo, quos quibusdam dolor quisquam nesciunt modi doloribus corporis explicabo quam in totam aspernatur corrupti provident esse earum odio tempore error alias.</p>
 </body>
 </html>
